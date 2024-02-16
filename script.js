37d6e99b3f741202899ba9535060e715079f674d
BokaCatraca <160217532+BokaCatraca@users.noreply.github.com>
2024-02-16 05:00:44 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Adiciona um evento de clique ao botão "Sim"
+document.getElementById('joinButton').addEventListener('click', function() {
+  // Redireciona para o link externo quando o botão "Sim" é clicado
+  window.location.href = 'https://chat.whatsapp.com/JIEWa1nhvXv9t131mNtFHO';
+});
+
+// Adiciona um evento de clique ao botão "Não"
+document.getElementById('declineButton').addEventListener('click', function() {
+  // Adicione aqui a ação desejada quando o botão "Não" é clicado
+  alert('ENTÃO VAI PRO CARALHO.');
+});
